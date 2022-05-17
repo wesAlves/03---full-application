e0ad50de19fd3f88881aa7d293c80f2dc436d026
Wes <wal2.designer@gmail.com>
2022-05-17 15:17:27 +0100

    [slug] posts all passing

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { GetStaticPaths, GetStaticProps } from 'next';
-
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -17,7 +16,7 @@ interface Post {
       url: string;
     };
     author: string;
-    Content: {
+    content: {
       heading: string;
       body: {
         text: string;
@@ -30,36 +29,40 @@ interface PostProps {
   post: Post;
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export default function Post({ post }: Post) {
-  // TODO
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
 
-  const { data, first_publication_date } = post;
+  const formatedDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
 
-  const content = data.Content;
-
-  const formatedDate = format(new Date(first_publication_date), 'dd MMM yyyy', {
-    locale: ptBR,
-  });
-
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
   return (
     <>
-      <img src={data.banner.url} alt="banner" />
-      <h1>{data.title}</h1>
+      <img src="data.banner.url" alt="banner" />
+      <h1>{post.data.title}</h1>
       <div>
         <ul>
           <li>4 min</li>
           <li>{formatedDate}</li>
-          <li>{data.author}</li>
+          <li>{post.data.author}</li>
         </ul>
       </div>
 
-      {content !== undefined &&
-        content.map(({ heading, body }) => {
+      {post.data.content !== undefined &&
+        post.data.content.map(({ heading, body }) => {
           return (
             <>
               <h2>{heading}</h2>
-              <p>{body.text}</p>
+              {body.map(b => {
+                return <p>{b.text}</p>;
+              })}
               {/* {body.map(bodyText => {
                 return <p>{bodyText.text}</p>;
               })} */}
@@ -74,29 +77,27 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient({});
   const posts = await prismic.getByType('posts');
 
-  const uid = posts.results.map(post => {
-    return post.uid;
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
   });
 
   return {
-    // paths: [{ params: { slug: uid[0] } }, { params: { slug: uid[1] } }],
-    paths: [],
-    // { params: { slug: posts.results[1].uid } },
-    fallback: 'blocking',
+    paths,
+    fallback: true,
   };
-  // TODO
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params;
+  const { uid } = params;
 
   const prismic = getPrismicClient({});
-  const response = await prismic.getByUID('posts', String(slug));
+  const response = await prismic.getByUID('posts', uid as string);
 
   const post = response;
 
-  return {
-    props: { post },
-    revalidate: 60 * 60 * 24 * 7,
-  };
+  return { props: { post }, revalidate: 1 };
 };
