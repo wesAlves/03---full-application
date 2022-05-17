c78cbbf31115a8871aa2b50f7b557d6e671f1f4c
Wes <wal2.designer@gmail.com>
2022-05-17 11:26:27 +0100

    _document done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,21 @@
-import Document from 'next/document';
+import Document, { Head, Html, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@400;600&family=Roboto:wght@400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
